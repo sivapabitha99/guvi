c9dd6eee6a0a0145ed8554893a8964893fc9afd7
sivapabitha99 <47381516+sivapabitha99@users.noreply.github.com>
2019-02-06 14:29:07 +0530

    Create pabi.java

---
+++ pabi.java
@@ -0,0 +1,19 @@
+ class Demo
+{
+    public static void main(String[] args) 
+    {
+        int number=3;
+        if(number > 0)
+        {
+            System.out.println( "positive number");
+        }
+        else if(number < 0)
+        {
+            System.out.println( "negative number");
+        }
+        else
+        {
+            System.out.println(" positive nor negative");
+        }
+    }
+}
