d19e52a50571d25b3d63b617c339807669ec6681
sivapabitha99 <47381516+sivapabitha99@users.noreply.github.com>
2019-02-06 15:13:14 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,13 @@
+public class Main
+{
+
+  public static void main(String args[])
+  {
+    int num;
+    num =3;
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
